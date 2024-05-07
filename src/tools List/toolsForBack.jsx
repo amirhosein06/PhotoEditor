df3905a01a3541fc3dbbc520943ef36f885f255f
amirhosein06 <amirhoseinzolfaghari06@gmail.com>
2024-05-07 18:47:17 +0330

    added handler founc for background

--- src/tools List/toolsForBack.jsx
+++ src/tools List/toolsForBack.jsx
@@ -1,13 +1,20 @@
 import { ToolsList,ToolItem,ToolsIcon } from "../components/createtools";
+import Context from '../components/context/context';
+import { useContext } from "react";
 
 const BackgroundTools = () => {
+    const context = useContext(Context);
+    
+    const openHandlerBack = (targetComponent)=>{
+        context.sethandleComponent(targetComponent);
+    };
     return ( 
     <ToolsList>
-     <ToolItem><ToolsIcon className="bi bi-image-fill"></ToolsIcon>تـنـظـیـم تـصـویـر</ToolItem>
-     <ToolItem><ToolsIcon className="bi bi-transparency"></ToolsIcon>جــلـوه</ToolItem>
-     <ToolItem><ToolsIcon className="bi bi-palette-fill"></ToolsIcon>رنـگ پـس زمـیـنـه</ToolItem>
-     <ToolItem><ToolsIcon className="bi bi-arrows-fullscreen"></ToolsIcon>ابـعـاد تـصـویـر</ToolItem>
-     <ToolItem><ToolsIcon className="bi bi-water"></ToolsIcon>بـافـت تـصـویـر</ToolItem>
+     <ToolItem onClick={()=>{openHandlerBack(context.handlerListState.AddPhoto)}}><ToolsIcon className="bi bi-image-fill"></ToolsIcon>تـنـظـیـم تـصـویـر</ToolItem>
+     <ToolItem onClick={()=>{openHandlerBack(context.handlerListState.AddFilter)}}><ToolsIcon className="bi bi-transparency"></ToolsIcon>جــلـوه</ToolItem>
+     <ToolItem onClick={()=>{openHandlerBack(context.handlerListState.BackColor)}}><ToolsIcon className="bi bi-palette-fill"></ToolsIcon>رنـگ پـس زمـیـنـه</ToolItem>
+     <ToolItem onClick={()=>{openHandlerBack(context.handlerListState.BackResizing)}}><ToolsIcon className="bi bi-arrows-fullscreen"></ToolsIcon>ابـعـاد تـصـویـر</ToolItem>
+     <ToolItem onClick={()=>{openHandlerBack(context.handlerListState.BackTexture)}}><ToolsIcon className="bi bi-water"></ToolsIcon>بـافـت تـصـویـر</ToolItem>
      <ToolItem><ToolsIcon className="bi bi-arrow-clockwise"></ToolsIcon>حـذف پـس زمـیـنـه</ToolItem>
     </ToolsList> 
     );
